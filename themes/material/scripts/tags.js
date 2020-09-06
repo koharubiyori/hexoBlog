befcc25a45324f0b8e5ad73b6291684918280af6
@Inori <2665611998@qq.com>
2020-09-06 22:15:54 +0800

    update

---
+++ themes/material/scripts/tags.js
@@ -0,0 +1,11 @@
+/**
+ * 警示块
+ * @param {['info' | 'success' | 'warning' | 'danger', string])} type 
+ * @param {string} content
+ */
+function alertTag([type = 'info', text], content) {
+  const alertContent = text || content
+  return `<div class="alertTag" data-type="${type}">${alertContent}</div>`
+}
+
+hexo.extend.tag.register('alert', alertTag, { ends: true })
